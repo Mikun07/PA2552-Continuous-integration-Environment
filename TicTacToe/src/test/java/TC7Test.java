e7db629e4da367f9a8ba4dfa9cacd55b4c889b59
DESKTOP-6A3D8OR\MANDY <mandela.ogunleye@gmail.com>
2025-03-09 05:35:53 +0100

    Fix

---
+++ TicTacToe/src/test/java/TC7Test.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import javax.swing.*;
+
+public class TC7Test { // No package declarations
+    private TicTacToe game;
+
+    @BeforeEach
+    void setUp() {
+        game = new TicTacToe('X'); // Initialize game with player 'X'
+        game.setVisible(true); // Ensure UI is loaded before interacting with buttons
+    }
+
+    @Test
+    void testPlayerMoves() {
+        JButton button1 = game.buttons[0][0]; // First move (X)
+        JButton button2 = game.buttons[1][1]; // Second move (O)
+
+        button1.doClick(); // Player 'X' clicks
+        assertEquals("X", button1.getText(), "Button should display 'X'");
+        assertEquals('O', game.getCurrentPlayer(), "Next turn should be 'O'");
+
+        button2.doClick(); // Player 'O' clicks
+        assertEquals("O", button2.getText(), "Button should display 'O'");
+        assertEquals('X', game.getCurrentPlayer(), "Turn should switch back to 'X'");
+    }
+}
