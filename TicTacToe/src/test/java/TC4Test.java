f1c02ed59c74e630d26e981a43b13d7397dd19ba
DESKTOP-6A3D8OR\MANDY <mandela.ogunleye@gmail.com>
2025-03-09 06:48:58 +0100

    no message

--- TicTacToe/src/test/java/TC4Test.java
+++ TicTacToe/src/test/java/TC4Test.java
@@ -13,12 +13,18 @@ public class TC4Test {
     }
 
     @Test
-    void testPreventRepeatedMove() {
+    void testPreventRepeatedMove() throws InterruptedException {
         JButton button = game.buttons[1][1]; // Select a button
-        button.doClick(); // Player 'X' clicks
+        button.doClick(); // Player 'X' clicks or moves
+
+        // Allow UI to update (only needed for CI/CD)
+        Thread.sleep(500); // Give time for UI to process
+
         assertEquals("X", button.getText(), "First move should be 'X'");
 
-        button.doClick(); // Try clicking again
+        button.doClick();  // Second click (invalid)
+        Thread.sleep(500); // Allow UI to update
+        
         assertEquals("X", button.getText(), "Button should not change after a second click");
         assertEquals('O', game.currentPlayer, "Turn should not switch after an invalid move");
     }
