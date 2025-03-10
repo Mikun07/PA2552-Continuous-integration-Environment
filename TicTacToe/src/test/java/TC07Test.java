840f4a87eab00dc6b2d4c86eebcc49a9e62625da
Festus-Olaleye Ayomikun <76745046+Mikun07@users.noreply.github.com>
2025-03-10 17:40:13 +0100

    TC-07 Fix

--- TicTacToe/src/test/java/TC07Test.java
+++ TicTacToe/src/test/java/TC07Test.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import javax.swing.*;
 
-public class TC7Test { 
+public class TC07Test { 
     private TicTacToe game;
 
     @BeforeEach
